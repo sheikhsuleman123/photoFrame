ac44fd83457762efceb83c752846d6a5ea4616b7
sheikhsuleman123 <mssheikh333@gmail.com>
2019-11-27 15:38:34 +0500

    title is done

---
+++ src/Components/Title.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../Components/Styling.css';
+
+export default function Title() {
+    return (
+        <div>
+            <h1 className="title">Photoframe</h1>
+        </div>
+    )
+}
