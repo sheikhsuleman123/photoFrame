3065e54254106869d734c26b0f87f8c53df5eae4
sheikhsuleman123 <mssheikh333@gmail.com>
2019-11-30 15:22:31 +0500

    converting array to state

--- src/App.js
+++ src/App.js
@@ -1,32 +1,37 @@
-import React from 'react';
+import React,{Component} from 'react';
 import Title from './Components/Title';
 import Photoframe from './Components/PhotoFrame';
 
-const allPhotos = [{
-  id: "0",
-  description: "Beautiful sea",
-  imageLink: "https://picsum.photos/800/600?image=14"
-  }, {
-  id: "1",
-  description: "Coffee cup",
-  imageLink: "https://picsum.photos/800/600?image=30"
-  }, {
-  id: "2",
-  description: "Freedom",
-  imageLink: "https://picsum.photos/800/600?image=50"
-  }, {
-  id: "3",
-  description: "Farm",
-  imageLink: "https://picsum.photos/800/600?image=85"
-  }]
-
-function App() {
-  return (
-    <div >
-     <Title  title={'Photoframe'}/>
-     <Photoframe photoblock={allPhotos} />
-    </div>
-  );
+export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      allPhotos : [{
+        id: "0",
+        description: "Beautiful sea",
+        imageLink: "https://picsum.photos/800/600?image=14"
+        }, {
+        id: "1",
+        description: "Coffee cup",
+        imageLink: "https://picsum.photos/800/600?image=30"
+        }, {
+        id: "2",
+        description: "Freedom",
+        imageLink: "https://picsum.photos/800/600?image=50"
+        }, {
+        id: "3",
+        description: "Farm",
+        imageLink: "https://picsum.photos/800/600?image=85"
+        }]
+    }
+  }
+  render() {
+    return (
+      <div>
+         <Title  title={'Photoframe'}/>
+     <Photoframe photoblock={this.state.allPhotos} />
+      </div>
+    )
+  }
 }
 
-export default App;
